102d1b5a8a55652ac428df3a62905ca56c4c0cd8
larsquix <97360134+larsquix@users.noreply.github.com>
2022-01-08 20:50:39 +0100

    css, images, js

---
+++ js/masonry.js
@@ -0,0 +1,12 @@
+var macyInstance = Macy({
+  container: '#masonry',
+  trueOrder: false,
+  waitForImages: false,
+  margin: 12,
+  columns: 3,
+  breakAt: {
+      940: 3,
+      520: 2,
+      400: 1
+    }
+});
